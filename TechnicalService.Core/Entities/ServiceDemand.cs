e03496d50a5afff8dbdf601e0f095fb4090df180
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-06-08 22:01:48 +0300

    Automapper Done

--- TechnicalService.Core/Entities/ServiceDemand.cs
+++ TechnicalService.Core/Entities/ServiceDemand.cs
@@ -5,19 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using TechnicalService.Core.Entities.Abstract;
 
-namespace TechnicalService.Core.Entities
+namespace TechnicalService.Core.Entities;
+
+public class ServiceDemand : BaseEntity<int>
 {
-    public class ServiceDemand : BaseEntity<int>
-    {
-        public string Name { get; set; }
-        public string SurName { get; set; }
-        public string Email { get; set; }
-        public string BuildingNo { get; set; }
-        public string FloorNo { get; set; }
-        public string DoorNo { get; set; }
-        public string Phone { get; set; }
-        public string Message { get; set; }
-        public string Adress { get; set; }
-        public string UserId { get; set; }
-    }
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public string Email { get; set; }
+    public string BuildingNo { get; set; }
+    public string FloorNo { get; set; }
+    public string DoorNo { get; set; }
+    public string Phone { get; set; }
+    public string Message { get; set; }
+    public string Address { get; set; }
+    public string? UserId { get; set; }
+
 }
